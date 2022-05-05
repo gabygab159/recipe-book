7fcb9948d6becb7639a02ce28f998b5f761456c3
Gabriel Chen <gabriel.jchen@gmail.com>
2022-05-04 21:18:23 -0400

    setup recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Good recipe',
+      'very delicious',
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flickr.com%2Fphotos%2Fmmm-yoso%2F45800475025&psig=AOvVaw2HaWqo-uhjspck_6uYLfo5&ust=1651799347496000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOCy2aKWx_cCFQAAAAAdAAAAABAD'
+    ),
+  ];
 
   constructor() {}
 
